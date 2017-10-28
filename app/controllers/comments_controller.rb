de446d96d2cdac01ba64d7c34f07f1c5461b7f6e
Adelina_Galunova <adelina.vitamine@gmail.com>
2017-10-28 17:42:12 +0600

    Add validations for comment. Add average score for photos. Change views for them.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,43 +1,42 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
   before_action :set_photo
 
-  def index
-    @comments = @photo.comments.order("created_at ASC")
-
-    respond_to do |format|
-      format.html { render 'comments/index' }
-    end
-  end
-
   def create
+    authorize! :create, Comment
     @comment = @photo.comments.build(comment_params)
     @comment.user_id = current_user.id
 
     if @comment.save
-      redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
     else
       flash[:alert] = "Check the comment form, something went wrong."
-      render root_path
+      redirect_to :back
     end
   end
 
   def destroy
     @comment = @photo.comments.find(params[:id])
-
+    authorize! :destroy, @comment
     if @comment.user_id == current_user.id
       @comment.delete
-			redirect_to root_path
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:text, :rate)
   end
 
   def set_photo
     @photo = Photo.find(params[:photo_id])
   end
 end
-
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,15 @@
 class Photo < ApplicationRecord
-	mount_uploader :image, PhotoUploader
-	belongs_to :user
-	has_many :comments, dependent: :destroy
-	validates :user_id, :title, :image, presence: true
+  mount_uploader :image, PhotoUploader
+  belongs_to :user
+  has_many :comments, dependent: :destroy
 
+  validates :user_id, :title, :image, presence: true
+
+  def average_score
+    average = 0.0
+    self.comments.each do |comment|
+     average += comment.rate
+    end
+    (average / self.comments.count).round(1)
+  end
 end
