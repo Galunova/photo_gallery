d2e2cd310852a37648f0f8ebf836bb6d728e3a1f
Adelina_Galunova <adelina.vitamine@gmail.com>
2017-10-28 12:46:17 +0600

    Add users. Add role to users. Add seeds for users.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+
+  def role?(r)
+	  role.include? r.to_s
+	end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create!(email: 'admin@example.com', password: 'adminpassword', role: 'admin', name: 'Admin')
+user = User.create!(email: 'yojin@example.com', password: 'yojinpassword', role: 'none', name: 'Yojin')
